d3b7208800aeb36a63ee36958e477edd6332eed8
RachelJessicaTan <rcljsstn@gmail.com>
2026-06-05 18:45:43 +0700

    Added: LoginPage

--- lib/pages/LoadPage.dart
+++ lib/pages/LoadPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'LoginPage.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -63,12 +63,12 @@ class LoadPage extends StatelessWidget {
                     height: 56,
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SignUpPage(),
-                          ),
-                        );
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => const SignUpPage(),
+                        //   ),
+                        // );
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: darkButtonColor,
